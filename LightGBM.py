f9c030e1c54ed39f2a53bad41643b6050a909052
Güneş Balcı <gunesbalci66@hotmail.com>
2026-09-09 11:23:21 +0300

    Unnecessary functions are removed.

--- LightGBM.py
+++ LightGBM.py
@@ -6,21 +6,12 @@ import seaborn as sns
 import numpy as np
 import optuna
 from sklearn.metrics import roc_auc_score
-from dataset_encode import encode_train_test_datasets
-from dataset_preprocess import create_train_test_datasets
 from comparing import calc_visualize_result
 from sklearn.model_selection import StratifiedKFold
 
 import warnings
 warnings.filterwarnings("ignore")
 optuna.logging.set_verbosity(optuna.logging.WARNING)
-
-# EXTRACTING TRAIN AND TEST DATA FROM DATASET
-def extract_data(apply_target_encoding=True):
-    df = pd.read_csv("Dataset/in-vehicle-coupon-recommendation.csv")
-    X_train, X_test, y_train, y_test = create_train_test_datasets(df)
-    X_train, X_test = encode_train_test_datasets(X_train, X_test, y_train, apply_target_encoding)
-    return X_train, X_test, y_train, y_test
 
 def objective(trial, data):
     X_train, X_test, y_train, y_test = data
@@ -51,13 +42,6 @@ def objective(trial, data):
     score = roc_auc_score(y_test, y_pred_proba)
     
     return score
-
-def learn_best_params(data):
-    study = optuna.create_study(direction='maximize')
-    study.optimize(lambda trial: objective(trial, data), n_trials=50)
-
-    print(f"En iyi parametreler: {study.best_params}")
-    print(f"En iyi AUC Skoru: {study.best_value:.4f}")
 
 def train(data, print_on):
     X_train, X_test, y_train, y_test = data
@@ -132,9 +116,3 @@ def Kfold_CV(data):
     print(f"Her Fold'un AUC Skorları: {[round(s, 4) for s in cv_scores]}")
     print(f"Ortalama CV AUC Skoru: {np.mean(cv_scores):.4f} (+/- {np.std(cv_scores):.4f})")
     return np.mean(cv_scores)
-
-def get_best_model():
-    return LGBMClassifier(boosting_type='gbdt', n_estimators=582,
-        learning_rate=0.09135301766719553, max_depth=6, num_leaves=110,
-        min_child_samples=21, subsample=0.9841372878332021,
-        colsample_bytree=0.6359476359169153, verbosity=-1, random_state=42, objective='binary')
